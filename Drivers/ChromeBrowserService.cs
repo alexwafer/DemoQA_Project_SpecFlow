b755f1e5b1f94c40e228a4660ba8fdff39f847f7
AlexWafer <AlexWafer@DESKTOP-QE2QTI0>
2024-07-02 15:07:08 +0300

    cicd

--- Drivers/ChromeBrowserService.cs
+++ Drivers/ChromeBrowserService.cs
@@ -9,7 +9,20 @@ namespace DemoQA_Project_SpecFlow.Drivers
 
         public void SetUpDriver()
         {
-            Driver = new ChromeDriver();
+            //dotnet test ..dll --testparam=ciCd=true
+            var ciCd = Environment.GetEnvironmentVariable("ciCd");
+            if (ciCd != null)
+            {
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("--headless");
+                //aici sa faca instanta cu headless
+                Driver = new ChromeDriver(options);
+            }
+            else
+            {
+                //rulez fara headless
+                Driver = new ChromeDriver();
+            }
             Driver.Manage().Window.Maximize();
             Driver.Url = "https://demoqa.com/";
         }
